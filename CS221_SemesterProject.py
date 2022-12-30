32476f0476d0f2096b4700b3df4e929f9be4461d
Rehan Riaz <101092200+Rehannriaz@users.noreply.github.com>
2022-12-30 19:15:37 +0500

    final changes

--- CS221_SemesterProject.py
+++ CS221_SemesterProject.py
@@ -1,68 +1,86 @@
-import csv
-import numpy as np
-import random
-import pandas as pd
-import cv2 as cv2
-import scipy.stats as stats
-import io
-import os
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-from PIL import Image, ImageChops
-import networkx as nx
+'''
+Title: CS221-Prj-2021438-2021051-2021355-2021718
+Author: Muhammad Rehan - 2021438
+        Adeen Amir - 2021051
+        Muhammad Arsal - 2021355
+        Zaid Bin Muzammil - 2021718
+Brief:  implemented a set of clustering techniques
+
+'''
 
 
-def fileReader():
+import numpy as np                                           # np used for color-coded image  
+import random                                                # random used for randomizing rows while permuting
+import pandas as pd                                          # used to read csv file and used for dataframe.
+import cv2 as cv2                                            # cv2 used to save image of discretized
+import os                                                    # used to remove temporary image
+import matplotlib.pyplot as plt                              # used to save images for graphs     
+import matplotlib.cm as cm                                   # used in color map
+import networkx as nx                                        # used for producing graph images
+
+
+
+def fileReader():                                            # Function to read a csv file
+    # fileName = "winecsv.csv"
     fileName = "sampledataCSV.csv"
-    iFile = pd.read_csv(fileName)
+    iFile = pd.read_csv(fileName)                           # reads the file into iFile directly through pandas library
 
     return (iFile)
 
 
-def displayColor(fileName, array, outputFileName):
+
+
+def displayColor(fileName, array, outputFileName):          # Function to display a color coded image.
     plt.imsave(fileName, np.array(array).reshape(
-        len(array), len(array[0])), cmap=cm.gray)
+        len(array), len(array[0])), cmap=cm.gray)           # using PLT we are able to produce a bitmap of the discretized matrix 
+                                                            
 
     img = cv2.imread(fileName)
     b, g, r = cv2.split(img)
     zeros_ch = np.zeros(img.shape[0:2], dtype="uint8")
-    greenImg = cv2.merge([zeros_ch, g, zeros_ch])
+                                                            # Eliminates all the other color elements except green.
+    greenImg = cv2.merge([zeros_ch, g, zeros_ch])           # forms the green image
     os.remove(fileName)
     cv2.imwrite(outputFileName, greenImg)
 
-
-def PearsonCorrelation(iFile):
+                                                            
+def PearsonCorrelation(iFile):                              # Function to find the Pearson correlation coefficient between each pair of rows in a dataset.
     corrMat = iFile
 
-    corrMat = pd.DataFrame(corrMat.transpose())
-    corrMat = corrMat.corr(method="pearson")
+    corrMat = pd.DataFrame(corrMat.transpose())             # transposes the data.
+    corrMat = corrMat.corr(method="pearson")                # applies pearson on the transposed data
     return corrMat
 
 
-def discretizedMatrix(matArray):
+
+
+def discretizedMatrix(matArray):                            # Function to discretize a matrix.
     arrMean = matArray.mean()
-    matArrayMean = arrMean.to_numpy()
+    matArrayMean = arrMean.to_numpy()                       # calculating mean of each column.
+                                                            # comparing each value with it and discretizing it accordingly.
     for i in range(len(matArray)):
         for j in range(len(matArray[i])):
-            if matArray[i][j] <= matArrayMean[i]:   # PROBLEM HERE
+            if matArray[i][j] <= matArrayMean[i]:
                 matArray[j][i] = 0
                 matArray[i][j] = 0
             else:
                 matArray[i][j] = 1
                 matArray[j][i] = 1
 
-    for i in range(len(matArray)):                   # # removable if problem fixed
+    for i in range(len(matArray)):
         for j in range(len(matArray[i])):
             if (matArray[i][j] == 1):
                 matArray[i][j] = 0
             else:
                 matArray[i][j] = 1
 
-    plt.imsave('discretizedMatrix.png', np.array(matArray).reshape(
+    plt.imsave('discretizedMatrix.png', np.array(matArray).reshape(  
         len(matArray), len(matArray[0])), cmap=cm.gray)
 
 
-def colorCoded(similarityMatrix):
+
+
+def colorCoded(similarityMatrix):                           # Function to display the color coded image of a discretized matrix.
     maxArr = []
     maxValue = -1
 
@@ -73,7 +91,7 @@ def colorCoded(similarityMatrix):
 
         maxArr.append(maxValue)
 
-    for i in range(len(similarityMatrix)):
+    for i in range(len(similarityMatrix)):                  # following steps for a colorcoded image
         for j in range(len(similarityMatrix[i])):
             similarityMatrix[j][i] = similarityMatrix[j][i]/maxArr[i]
             similarityMatrix[j][i] = similarityMatrix[j][i]*255
@@ -81,21 +99,35 @@ def colorCoded(similarityMatrix):
     displayColor('greentest.png', similarityMatrix, "GreenColorCoded1.png")
 
 
-def Task1(iFile):
+
+def Task1(iFile):                                           # Function to execute the Task1 of the program.
     corrMat = PearsonCorrelation(iFile)
     corrMean = corrMat.mean()
     similarityMatrix = corrMat.copy()
     discretizedMatrix(corrMat)
     colorCoded(similarityMatrix)
+    print("Task 1 completed...")
 
     return corrMat
 
 
-def clusterRecover(permutedArray):
+                                                            # Function to randomly shuffle the individual rows of the data matrix,  
+def permuteMatrix(arrayDF):                                 # Takes a dataframe as a parameter.
+    arrayMat = arrayDF.to_numpy()
+    np.random.shuffle(arrayMat)
+    print(arrayMat)
+    plt.imsave('shuffle.png', np.array(arrayMat).reshape(
+        len(arrayMat), len(arrayMat[0])), cmap=cm.gray)
+    return (arrayMat)
+
+
+
+
+def clusterRecover(permutedArray):                          # Function to recover the image clusters using Signature technique.
     sumArr = []
     meanArr = []
     MultSumArr = []
-    for i in range(len(permutedArray)):
+    for i in range(len(permutedArray)):                     # clusters recovered using the given steps
         sum = 0
         for j in range(len(permutedArray[i])):
             sum += permutedArray[i][j]
@@ -105,97 +137,83 @@ def clusterRecover(permutedArray):
 
     permutedDF = pd.DataFrame(permutedArray)
     permutedDF["Signature"] = MultSumArr
+                                                            # sorting on the basis of signature values.
     permutedDF = permutedDF.sort_values("Signature")
     permutedDF = permutedDF.drop(columns=["Signature"])
     return (permutedDF)
 
 
-def Task2(iFile):
-    permutedMat = permuteMatrix(iFile)
+
+
+def Task2(iFile):                                           # Function to execute the Task2 of the program.
+    permutedMat = permuteMatrix(iFile)                      # Takes original dataset as parameter
     permutedCorrMat = PearsonCorrelation(permutedMat)
     signaturePermutedMat = clusterRecover(permutedMat)
     Task1(signaturePermutedMat)
-    # return (permutedMat)
+    print("Task 2 completed...")
 
 
-def permuteMatrix(arrayDF):
-    arrayMat = arrayDF.to_numpy()
-    np.random.shuffle(arrayMat)
-    print(arrayMat)
-    plt.imsave('shuffle.png', np.array(arrayMat).reshape(
-        len(arrayMat), len(arrayMat[0])), cmap=cm.gray)
-    return (arrayMat)
 
-def makeGraph(permutedCorrMat, GraphCounter):
-    sumArr = permutedCorrMat.sum()
-    MaxWeightofNode = sumArr.argmax()
 
-    DG = nx.DiGraph()
-    DG.add_node(MaxWeightofNode)
+def makeGraph(permutedCorrMat, GraphCounter):               # Function to create a weighted graph out of a permuted data matrix.
+    sumArr = permutedCorrMat.sum()                          # sum of the weights of each edge connected to each node.
+    MaxWeightofNode = sumArr.argmax()                       # Finding max weight of each node.
+    DG = nx.DiGraph()                                       # Creating a weighted graph.
+    DG.add_node(MaxWeightofNode)                            # add the maxWeight of Node onto the graph
 
     for i in range(len(permutedCorrMat)):
         if permutedCorrMat[MaxWeightofNode][i] != 0 and MaxWeightofNode != i:
-            DG.add_node(i)
-            DG.add_edge(MaxWeightofNode, i)
+            DG.add_node(i)                                  # added the neighbor nodes of maxWeight to graph 
+            DG.add_edge(MaxWeightofNode, i)                 # added the edges of neighbor to maxWeight
+
             
-            permutedCorrMat[MaxWeightofNode][i] = 0
-            permutedCorrMat[i][MaxWeightofNode] = 0
-            for j in range(len(permutedCorrMat[i])):
+            permutedCorrMat[MaxWeightofNode][i] = 0         # Setting the weight of each edge to a node zero      
+            permutedCorrMat[i][MaxWeightofNode] = 0         # after recovering a cluster form it.
+            for j in range(len(permutedCorrMat[i])):        # Setting the neighbors row and column to zero
                 permutedCorrMat[i][j] = 0
                 permutedCorrMat[j][i] = 0
 
-    nx.draw(DG)
-    FileName = "Graphpng\graph"+str(GraphCounter)+".png"
-    DG.clear()
-    plt.savefig(FileName)
-    plt.clf()
+    nx.draw(DG)                                             # Draw the graph
+    FileName = "Graphpng\graph"+str(GraphCounter)+".png"    # Filename of the graph 
+    DG.clear()                                              # clear the graph
+    plt.savefig(FileName)                                   # saving each graph.
+    plt.clf()                                               # clearing the plt screen
     return (permutedCorrMat)
 
 
-def getThreshold(TH):
-    Threshold=TH
+def getThreshold(TH):                                       # used in GUI to send threshold for task 3
+    Threshold = TH
     return float(Threshold)
 
 
-def Task3(permutedDataSet,threshold):
+
+
+def Task3(permutedDataSet, threshold):                     # Functtion to execute Task3, takes permutedDataSet and threshold as parameters 
     permutedCorrMat = PearsonCorrelation(permutedDataSet)
     print(permutedCorrMat)
     minimumWeight = permutedCorrMat.min()
     minimumWeight = minimumWeight.min()
-
     print(minimumWeight)
-    # threshold = float(input("Enter a threshold,Minimum Value is " +
-    #                   str(minimumWeight)+", and Max of 1: "))
 
-    if threshold < float(minimumWeight):
-        threshold = float(minimumWeight)
+
+    if threshold < float(minimumWeight):                   # error handling if user inputs threshold out of range
+        threshold = float(minimumWeight)                
     elif threshold > 1:
         threshold = 1
-    print("workss")
 
-    for i in range(len(permutedCorrMat)):
+    print("loading...")
+    for i in range(len(permutedCorrMat)):                  
         for j in range(len(permutedCorrMat[i])):
-            if permutedCorrMat[i][j] < threshold:
+            if permutedCorrMat[i][j] < threshold:         # equates all the values below the threshold to zero
                 permutedCorrMat[i][j] = 0
             if i == j:
-                permutedCorrMat[i][j] = 0
+                permutedCorrMat[i][j] = 0                 # equates diagonals to zero
 
-    k = 0
     for i in range(len(permutedCorrMat)):
         sumArr = permutedCorrMat.sum()
         MaxWeightofNode = sumArr.argmax()
         if (sumArr[MaxWeightofNode] == 0):
             break
-        permutedCorrMat = makeGraph(permutedCorrMat, i)
-        
+        permutedCorrMat = makeGraph(permutedCorrMat, i)  # makeGraph function repeatedly called until no more clusters are left
 
-    print("done")
-
-# def main():
-#     Task1(fileReader())
-#     # Task2(fileReader())
-#     # Task3(permuteMatrix(fileReader()))
-
-
-
-# main()
+    print("Task 3 completed...")
